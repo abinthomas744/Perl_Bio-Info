e1a66a8cf0729ecac9f85743f168338d0e4595c4
abinthomas744 <43981681+abinthomas744@users.noreply.github.com>
2020-01-14 20:39:24 +0530

    Rev DNA

--- DNAConcat.pl
+++ DNAConcat.pl
@@ -3,7 +3,7 @@ $DNA1='AGTAGTGTCC';
 $DNA2='GGTCCAATAG';
 print"DNA1:$DNA1\n";
 print"DNA2:$DNA2\n";
-$DNAConcat="$DNA1,$DNA2";
+$DNAConcat="$DNA1$DNA2";
 print"DNAConcat:$DNAConcat\n";
 exit
 
--- RNATrans.pl
+++ RNATrans.pl
@@ -1,9 +1,9 @@
 #!/usr/bin/perl
 $DNA='AGTAGTGTCCGGTCCAATAG';
-print"DNA:$DNA\n";
+print"DNA=$DNA\n";
 
 $RNA=$DNA;
 $RNA=~s/T/U/g;
-print"RNA=:$RNA\n";
+print"RNA=$RNA\n";
 exit
 
