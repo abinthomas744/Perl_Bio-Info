a4b9415928fa7b320a230ec95026f3983a008006
abinthomas744 <43981681+abinthomas744@users.noreply.github.com>
2020-01-14 19:14:51 +0530

    Add files via upload

---
+++ DNAConcat.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+$DNA1='AGTAGTGTCC';
+$DNA2='GGTCCAATAG';
+print"DNA1:$DNA1\n";
+print"DNA2:$DNA2\n";
+$DNAConcat="$DNA1,$DNA2";
+print"DNAConcat:$DNAConcat\n";
+exit
+
---
+++ Perl.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+print"Hello!\n";
+$x=10;
+$y=20;
+print"1st Variable:$x\n";
+print"2nd Variable:$y\n";
+$z=$x+$y;
+print"Sum:$z\n";
+$q=$y-$x;
+print"Difference:$q\n";
+EXIT
