4c17e43c897a86fca0db9e75776f6cc5ceb86e54
bmenitrr32/2 <abinthomas744@gmail.com>
2020-02-18 16:48:05 +0530

    LAB5

---
+++ LAB5/biopython_2.py
@@ -0,0 +1,18 @@
+import Bio.Alphabet
+from Bio.Alphabet import IUPAC
+from Bio.Seq import Seq
+pro_extend_dna=IUPAC.ExtendedIUPACProtein.letters;
+print(pro_extend_dna);
+print("\n"); 
+
+seq1=Seq('CCGGGTT.Bio.Alphabet.IUPAC.unambiguous_dna');
+print("seq1");
+
+seq.trancribe=seq1.ma_transcribe();
+print(seq1.ma_transcribe());
+
+ma_seq=Seq("CCGGGUU",Bio.Alphabet.IUPAC.unambiguous_ma);
+print(ma_seq);
+
+ma_transcribe=ma_transcribe();
+print(unambiguous_ma);
