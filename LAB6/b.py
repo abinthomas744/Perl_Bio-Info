7d7f5fa042c0619467754aaa70e5993cf61f3829
Abin <abinthomas744@gmail.com>
2020-03-15 16:21:48 +0530

    More work to do here

--- LAB6/b.py
+++ LAB6/b.py
@@ -1,47 +1,8 @@
-my_seq=Seq("AGCTTCCATTTGGGTCATGATCC")
-
-print(my_seq.complement());
-my_seq.count("T")
-
-GC_count=100*float(my_seq.count("G"))+my_seq.count("C")/len(my_seq);
-print(GC_count);
-
-my_seq1=Seq("GATTATTTCCCCGCGCCCAGTCAAGGTAGTGCCATAACCGTCCCTG",IUPAC.unambigous_dna);
-print(my_seq1[4:12]);
-
-print(my_seq[0::3]);
-print(my_seq[1::3]);
-print(my_seq[2::3]);
-print(my_seq[::-1]);
-
-#Concatenation
-protein_seq=Seq("EVRNAK",IUPAC.protein);
-dna_seq=Seq("ACGT",IUPAC.unambigous_dna);
-print(protein_seq+dna_seq);
-
-from Bio.Alphabet import generic_alphabet;
-protein_seq.alphabet=generic_alphabet;
-dna_seq.alphabet=generic_alphabet;
-print(protein_seq+dna_seq);
-
-from Bio.Alphabet import generic_nucleotide
-nuc_seq=Seq("GATCGATGC",generic_nucleotide);
-dna_seq=Seq("ACGT",IUPAC.unambigous_dna);
-print(nuc_seq+dna_seq);
-
-from Bio.Alphabet import generic_dna
-list_of_seqs=[Seq("ACGT",generic_dna),Seq("AACC",generic_dna),Seq("GGTT",generic_dna)]
-
-concatenated=Seq("",generic_dna)
-for s in list_of_seqs:
-    concatenateda+=s 
-    print(concatenated);
-
 from Bio.Seq import Seq
 from Bio.Alphabet import IUPAC
 my_seq=Seq("GCCATTGTAATGGAATTAGTGGGTAACCCAGGGTAAACCTACCACCCCAGCCACCTCAG",IUPAC.unambiguous_dna)
 
-# my_seq[5]="G"
+#my_seq[5]="G" This line won't work becuase the object(Seq) is immutable
 from Bio.Seq import MutableSeq
 
 mutable_seq=MutableSeq("GCCATTGTAATGGAATTAGTGGGTAACCCAGGGTAAACCTACCACCCCAGCCACCTCAG",IUPAC.unambiguous_dna)
@@ -54,3 +15,44 @@ k=my_seq.count('G')
 mut_newseq=my_seq.tomutable()
 for i in range(k):
     mut_newseq.remove("G")
+
+my_seq=Seq("AGCTTCCATTTGGGTCATGATCC")
+
+print(my_seq.complement());
+my_seq.count("T")
+
+GC_count=100*float(my_seq.count("G"))+my_seq.count("C")/len(my_seq);
+print(GC_count);
+
+
+#Slicing a sequence
+my_seq1=Seq("GATTATTTCCCCGCGCCCAGTCAAGGTAGTGCCATAACCGTCCCTG",IUPAC.unambiguous_dna);
+print(my_seq1[4:12]);
+
+print(my_seq[0::3]);
+print(my_seq[1::3]);
+print(my_seq[2::3]);
+print(my_seq[::-1]);
+
+#Concatenation
+protein_seq=Seq("EVRNAK",IUPAC.protein);
+dna_seq=Seq("ACGT",IUPAC.unambiguous_dna);
+#print(protein_seq+dna_seq);
+
+from Bio.Alphabet import generic_alphabet;
+protein_seq.alphabet=generic_alphabet;
+dna_seq.alphabet=generic_alphabet;
+print(protein_seq+dna_seq);
+
+from Bio.Alphabet import generic_nucleotide
+nuc_seq=Seq("GATCGATGC",generic_nucleotide);
+dna_seq=Seq("ACGT",IUPAC.unambiguous_dna);
+print(nuc_seq+dna_seq);
+
+from Bio.Alphabet import generic_dna
+list_of_seqs=[Seq("ACGT",generic_dna),Seq("AACC",generic_dna),Seq("GGTT",generic_dna)]
+
+concatenated=Seq("",generic_dna)
+for s in list_of_seqs:
+    concatenated+=s 
+    print(concatenated);
